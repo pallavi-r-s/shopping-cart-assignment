1aa4293529354b97a7543746a3a7963cff46097e
Pallavi Suryavanshi <palsurya@WKMZT4D4F07A.global.publicisgroupe.net>
2022-03-24 12:19:59 +0530

    html header abd footer updated in every files

---
+++ client/src/product.js
@@ -0,0 +1,9 @@
+
+let carts = document.querySelectorAll('.addcart');
+//console.log(carts);
+// for(let i=0;i<carts.length;i++){
+//     carts[i].addEventListener('click',()=>{
+//         console.log("aded to cart")
+//     })
+// }
+
